f12619d7d497d9faed94497f8a4914e3e60ce82b
Vlad-Yan <vladik_yanchevskiy@mail.ru>
2022-11-24 11:43:59 +0500

    #1 highlight add/edit form in modal window

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-"# JSMovieList-App" 
+# <div align="center">JSMovieList-App</div>
+
+ <div align="center"> 
+<a href="https://vlad-yan.github.io/JSMovieList-App/">View here</a>
+</div>
+
+### Description
+Application for maintaining a list of movies in JavaScript.
+Functional:
+- Data storage in LocalStorage;
+- Add/Remove/edit movies;
+- Add movies to favorites;
+- Processing error.
+
+### Screenshots
+![image](https://user-images.githubusercontent.com/93535054/203711927-b504b0fc-4ceb-482b-a4f1-a7cc87693418.png)
+![image](https://user-images.githubusercontent.com/93535054/203712073-fca2534a-b628-482d-8002-33c1db3b282f.png)
+![image](https://user-images.githubusercontent.com/93535054/203048376-c832b8c4-6f7f-499b-807d-2cec6f9221f0.png)
+
+***
+
+<div align="center">
+    2022 | <a href="https://github.com/Vlad-Yan"> Vlad-Yan </a>
+</div>
--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
             favorite: 'false'
         },
         {
-            title: 'Джон Картер ',
+            title: 'Джон Картер',
             genre: 'Фантастика',
             url: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/76aec0f0-bc47-4715-9818-73d3687da49f/300x450',
             favorite: 'false'
@@ -40,237 +40,190 @@ window.addEventListener('DOMContentLoaded', () => {
         },
     ]
 
-    let movies = getMovies(exampleMovies);
-    addMovie();
-    showMovies(movies);
-    delMovie();
-    editMovie();
+    getMovies(exampleMovies);
     favoritesMovies();
-    filterMovies();
-
-
-    function getMovies (exampleMovies) {
-        let movies;
-
-        if (localStorage.getItem('movies'))  {
-            movies = JSON.parse(localStorage.getItem('movies'));
-        } else {
-            localStorage.setItem('movies', JSON.stringify(exampleMovies)); 
-            movies = JSON.parse(localStorage.getItem('movies'));
-        }
-
-        return movies;
-    }
-
-
-    function addMovie () {
-        const newMovieForm = document.querySelector('.new-movie-form');
-
-        newMovieForm.addEventListener('submit', (e) => {
-            let movies = JSON.parse(localStorage.getItem('movies'));
-            e.preventDefault();
-            const movie = {
-                title: e.target.elements.title.value,
-                genre: e.target.elements.genre.value,
-                url: e.target.elements.url.value,
-                favorite: false
-            }
-
-            if (!movie.title || !movie.genre || !movie.url) {
-                alert("Заполните все поля!");
-            } else if(movies.find(m => m.title === movie.title)) {
-                alert("Такой фильм уже добавлен!");
-            } else {
-                movies.push(movie);
-                localStorage.setItem('movies', JSON.stringify(movies));
-                e.target.reset();
-                filterMovies();
-            }
-        });
-    }
-
-
-    function showMovies(data) {
-        const moviesEl = document.querySelector('.movies');
-        document.querySelector(".movies").innerHTML = "";
-
-        data.forEach(movie => {
-            let favoritButton;
-
-            if (movie.favorite === true) {
-                favoritButton = '<button data-action="favorit" class="no-favorit">Убрать из избранного</button>';
-            } else {
-                favoritButton = '<button data-action="favorit" class="favorit">Добавить в избранное</button>';
-            }
-
-            const movieEl = document.createElement("div");
-            movieEl.classList.add("movie");
-            movieEl.innerHTML = `
-                <div class="content">
-                <img src="${movie.url}" alt="movie">
-                <div class="movie-description">
-                    <p>Название фильма:</p>
-                    <input 
-                    id="title"
-                    type="text" 
-                    class="text" 
-                    placeholder="Введите название фильма"
-                    value="${movie.title}"
-                    readonly/>
-                    <p>Жанр:</p>
-                    <input 
-                    id="genre"
-                    type="text" 
-                    class="text" 
-                    placeholder="Введите жанр фильма"
-                    value="${movie.genre}"
-                    readonly/>
-                    <input
-                    id="url"
-                    type="text" 
-                    class="text hide" 
-                    placeholder="Введите URL обложки фильма"
-                    value="${movie.url}"
-                    />
-                </div>
-                </div>
-                <div class="actions">
-                    ${favoritButton}
-                    <button data-action="edit" class="edit">Изменить</button>
-                    <button data-action="delete" class="delete">Удалить</button>
-                </div>
-                `;
-            moviesEl.appendChild(movieEl);
-        });
-    }
-
-
-    function delMovie() {        
-        let deleteButton = document.querySelector('.movies');
-
-            deleteButton.addEventListener('click', (e) => {
-                let movies = JSON.parse(localStorage.getItem('movies'));
-
-                if (e.target.dataset.action === 'delete'){
-                    const title = e.target.closest('.movie').querySelector('#title').value;
-                    movies = movies.filter(m => m.title != title); 
-                    localStorage.setItem('movies', JSON.stringify(movies));
-                    filterMovies();
-                }
-        });
-    }
-
-    
-    function editMovie() {
-        let editButton = document.querySelector('.movies');
-
-        editButton.addEventListener('click', (e) => {
-
-            if (e.target.dataset.action === 'edit'){
-
-                let input = e.target.closest('.movie').querySelectorAll('input')
-                let title = e.target.closest('.movie').querySelector('#title');
-                let genre = e.target.closest('.movie').querySelector('#genre');
-                let url = e.target.closest('.movie').querySelector('#url');
-                let movies = JSON.parse(localStorage.getItem('movies'));
-                let index = movies.findIndex(movie => movie.title === title.value)
-
-                title.removeAttribute('readonly');
-                genre.removeAttribute('readonly');
-                url.classList.remove('hide');
-
-                let dataTitle = title.value;
-                let dataGenre = genre.value;
-                let dataUrl = url.value;
-                
-                input.forEach(input => {
-                    input.addEventListener('blur', (e) => {
-                        title.setAttribute('readonly', true);
-                        genre.setAttribute('readonly', true);
-                        url.classList.add('hide');
-
-                        if (!title.value || !genre.value || !url.value) {
-                            alert("Пожалуйста, не оставляйте поля пустыми!");
-                            title.value = dataTitle;
-                            genre.value = dataGenre;
-                            url.value = dataUrl;
-                        } else if(movies.find(m => m.title === title.value) && movies[index].title !== title.value) {
-                            alert("Такой фильм уже добавлен!");
-                            title.value = dataTitle;
-                        } else {
-                            let movie = {
-                                title: title.value,
-                                genre: genre.value,
-                                url: url.value,
-                                favorite: movies[index].favorite
-                            }
-                            movies[index] = movie;
-                            localStorage.setItem('movies', JSON.stringify(movies));
-                            filterMovies();
-                        }
-                    })
-                });
-            }
-        });
-    }
-
-
-    function favoritesMovies() {
-        let favoritButton = document.querySelector('.movies');
-        
-        favoritButton.addEventListener('click', (e) => {
-            let movies = JSON.parse(localStorage.getItem('movies'));
-
-            if (e.target.dataset.action === 'favorit'){
-                let title = e.target.closest('.movie').querySelector('#title');
-                let index = movies.findIndex(movie => movie.title === title.value)
-                if (e.target.classList.item(0) === 'favorit') {
-                    e.target.classList.remove('favorit');
-                    e.target.classList.add('no-favorit');
-                    e.target.textContent = "Убрать из избранного";
-                    movies[index].favorite = true;
-                } else {
-                    e.target.classList.remove('no-favorit');
-                    e.target.classList.add('favorit');
-                    e.target.textContent = "Добавить в избранное";
-                    movies[index].favorite = false;
-                }
-                localStorage.setItem('movies', JSON.stringify(movies));
-                filterMovies()
-            }
-        }
-    )};
-
-    
-    function filterMovies() {
-        let movies = JSON.parse(localStorage.getItem('movies'));
-        let btnFavorit = document.querySelectorAll('.btnFavorit');
-        let active = document.querySelector('.active');
-
-        if (active.outerText === "Избранные") {
-            movies = movies.filter(movie => movie.favorite === true);
-            showMovies(movies);
-        } else {
-            showMovies(movies);
-        }
-
-        btnFavorit.forEach(btn => {
-            btn.addEventListener('click', (e) => {
-                if(btn.outerText === "Все"){
-                    btn.classList.add("active");
-                    btn.nextElementSibling.classList.remove("active");
-                    movies = JSON.parse(localStorage.getItem('movies'));
-                } else {
-                    btn.classList.add("active");
-                    btn.previousElementSibling.classList.remove("active");
-                    movies = movies.filter(movie => movie.favorite === true)
-                }
-            showMovies(movies);        
-
-            }
-        )});
-    }
 });
+
+const NAME_LOCALSTORAGE = 'movies';
+let MODAL_INDEX = document.querySelector('.index');
+const MODAL_TITLE = document.querySelector('#exampleModalLabel');
+const MODAL_SUBMIT = document.querySelector('#new-movie-submit');
+
+
+function getMovies (exampleMovies) {
+    if (localStorage.getItem(NAME_LOCALSTORAGE) == null)  {
+        localStorage.setItem(NAME_LOCALSTORAGE, JSON.stringify(exampleMovies));
+    }
+    showMovies(JSON.parse(localStorage.getItem(NAME_LOCALSTORAGE)));
+}
+
+
+function addMovie (data) {
+    let movies = JSON.parse(localStorage.getItem(NAME_LOCALSTORAGE));
+    let index = MODAL_INDEX.textContent;
+    
+    const movie = {
+        title: data.elements.title.value,
+        genre: data.elements.genre.value,
+        url: data.elements.url.value,
+        favorite: false
+    }
+    if (!movie.title || !movie.genre || !movie.url) {
+        alert("Заполните все поля!");
+    }
+
+    if (index === "-1") {
+      if(movies.find(m => m.title === movie.title)) {
+            alert("Такой фильм уже добавлен!");
+        } else {
+            movies.push(movie);
+            localStorage.setItem(NAME_LOCALSTORAGE, JSON.stringify(movies));
+            data.reset();
+            filterMovies();
+            document.querySelector('.btn-close').click();
+        }
+
+    } else {
+        movie.favorite = movies[index].favorite;
+        if(movies.find(m => m.title === movie.title) && movies[index].title !== movie.title) {
+            alert("Такой фильм уже создан!");
+        } else {
+            movies[index] = movie;
+            localStorage.setItem(NAME_LOCALSTORAGE, JSON.stringify(movies));
+            data.reset();
+            filterMovies();
+            document.querySelector('.btn-close').click();
+        }
+    }
+}
+
+
+function showMovies(data) {
+    const moviesEl = document.querySelector('.movies');
+    document.querySelector(".movies").innerHTML = "";
+
+    data.forEach(movie => {
+        let favoritButton;
+
+        if (movie.favorite === true) {
+            favoritButton = '<button data-action="favorit" class="no-favorit">Убрать из избранного</button>';
+        } else {
+            favoritButton = '<button data-action="favorit" class="favorit">Добавить в избранное</button>';
+        }
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+        movieEl.innerHTML = `
+            <div class="content">
+            <img src="${movie.url}" alt="movie">
+            <div class="movie-description">
+                <p>Название фильма:</p>
+                <p class="title">${movie.title}</p>
+                <p>Жанр:</p>
+                <p>${movie.genre}</p>
+            </div>
+            </div>
+            <div class="actions">
+                ${favoritButton}
+                <button data-action="edit" class="edit" onclick="modalEdit('${movie.title}', '${movie.genre}', '${movie.url}')" data-bs-toggle="modal" data-bs-target="#exampleModal">Изменить</button>
+                <button data-action="delete" class="delete" onclick="delMovie('${movie.title}')">Удалить</button>
+            </div>
+            `;
+        moviesEl.appendChild(movieEl);
+    });
+}
+
+
+function delMovie(title) {        
+    let movies = JSON.parse(localStorage.getItem(NAME_LOCALSTORAGE));
+    movies = movies.filter(m => m.title != title); 
+    localStorage.setItem(NAME_LOCALSTORAGE, JSON.stringify(movies));
+    filterMovies();
+}
+
+
+function favoritesMovies() {
+    let favoritButton = document.querySelector('.movies');
+    
+    favoritButton.addEventListener('click', (e) => {
+        let movies = JSON.parse(localStorage.getItem(NAME_LOCALSTORAGE));
+
+        if (e.target.dataset.action === 'favorit'){
+            let title = e.target.closest('.movie').querySelector('.title');
+            console.log(e.target.closest('.movie'))
+
+            let index = movies.findIndex(movie => movie.title === title.textContent)
+
+            if (e.target.classList.item(0) === 'favorit') {
+                e.target.classList.remove('favorit');
+                e.target.classList.add('no-favorit');
+                e.target.textContent = "Убрать из избранного";
+                movies[index].favorite = true;
+            } else {
+                e.target.classList.remove('no-favorit');
+                e.target.classList.add('favorit');
+                e.target.textContent = "Добавить в избранное";
+                movies[index].favorite = false;
+            }
+            localStorage.setItem(NAME_LOCALSTORAGE, JSON.stringify(movies));
+            filterMovies()
+        }
+    }
+)};
+
+
+function filterMovies() {
+    let movies = JSON.parse(localStorage.getItem(NAME_LOCALSTORAGE));
+    let btnFavorit = document.querySelectorAll('.btnFavorit');
+    let active = document.querySelector('.active');
+
+    if (active.outerText === "Избранные") {
+        movies = movies.filter(movie => movie.favorite === true);
+        showMovies(movies);
+    } else {
+        showMovies(movies);
+    }
+
+    btnFavorit.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            if(btn.outerText === "Все"){
+                btn.classList.add("active");
+                btn.nextElementSibling.classList.remove("active");
+                movies = JSON.parse(localStorage.getItem(NAME_LOCALSTORAGE));
+            } else {
+                btn.classList.add("active");
+                btn.previousElementSibling.classList.remove("active");
+                movies = movies.filter(movie => movie.favorite === true)
+            }
+        showMovies(movies);        
+
+        }
+    )});
+}
+
+
+function modalAdd() {
+    MODAL_TITLE.textContent = "Добавить фильм";
+    MODAL_SUBMIT.value = "Добавить фильм";
+    MODAL_INDEX.textContent = "-1";
+}
+
+function modalEdit(title, genre, url) {
+    MODAL_TITLE.textContent = "Изменить фильм";
+    MODAL_SUBMIT.value = "Изменить фильм";
+
+    let titleInput = document.querySelector('#title-input');
+    let genreSelect = document.querySelector('#genre');
+    let urlInput = document.querySelector('#url-input');
+
+    titleInput.value = title;
+    genreSelect.value = genre;
+    urlInput.value = url;
+
+    let movies = JSON.parse(localStorage.getItem(NAME_LOCALSTORAGE));
+    let index = movies.findIndex(movie => movie.title === title)
+    MODAL_INDEX.textContent = index;
+}
 
 
 /* Фильмы для теста
